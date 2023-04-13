1d45a78b1ec011e12d2a641289667864b32ba3c8
dkedir <kedirdean@gmail.com>
2023-04-13 12:01:06 -0400

    Created helper file and started building v1 of the backtester

--- APE-Backtester/Backtester-v1.py
+++ APE-Backtester/Backtester-v1.py
@@ -1,29 +1,52 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from io import StringIO
+from datetime import timedelta, date, datetime
 import backtrader as bt
 import backtrader.feeds as btfeeds
+import itertools
 import pandas as pd
-from datetime import datetime, timedelta, date
-from matplotlib import warnings
-import sys
-import yfinance as yf
-import numpy as np
 import datetime as dt
-from BTFunctions import end_date_trading_days_only
+import helpers.backtraderhelpers as backtest
 import requests
 import json
-from yahooquery import Ticker
 import boto3
-from smart_open import smart_open
-import os
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+cerebro = bt.Cerebro()
+def set_cash():
+    cerebro.broker.setcash(200000.0)
+    print(cerebro.broker.getvalue())
 
-s3 = boto3.client('s3', aws_access_key_id = AWS_ACCESS_KEY_ID, aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
+def pull_data():
+    rawdata = backtest.s3_data()
+    start_date = datetime.strptime(rawdata['date'].values[1], '%Y-%m-%d %H:%M:%S')
+    end_date = backtest.end_date(start_date, 3)
+    symbol = rawdata['symbol'].values[1]
+    start_price = rawdata['regularMarketPrice']
+    polygon_df = backtest.polygon_data(symbol,start_date, end_date)
+    return start_date, end_date, symbol, start_price, polygon_df
 
-bucket_name = 'icarus-research-data'
-object_key = 'training_datasets/raw_1d_datasets/2023/01/02.csv'
-obj = s3.get_object(Bucket = bucket_name, Key = object_key)
-data = obj['Body'].read().decode('utf-8')
-print(data)
-df = pd.read_csv(data, sep=',')
-print(df)
+start_date, end_date, symbol, start_price, polygon_df = pull_data()
+
+data = bt.feeds.GenericCSVData(
+    dataname= f'/Users/ogdiz/Projects/APE-Research/APE-Backtester/APE-Backtester-Results/Testing_Research_Data_CSV_RBLX_2023-01-02 14/00/00.csv',
+    fromdate=start_date,
+    todate=end_date,
+    dtformat=('%Y-%m-%d %H:%M:%S'),
+    datetime=9,
+    open = 3,
+    high = 5,
+    low = 6,
+    close = 4,
+    volume =1, 
+    openinterest=-1,
+    reverse=False)
+
+cerebro.run()
+
+class backtraderstrat(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        #Logging function for the strat
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
