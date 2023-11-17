77a79da8fce93f4bf706ab83fb529ec746d294a0
Charles <cfmiller1397@gmail.com>
2023-11-17 13:01:30 -0500

    create time config

--- APE-Backtester/inv_backtesters/backtester_v3.py
+++ APE-Backtester/inv_backtesters/backtester_v3.py
@@ -71,21 +71,14 @@ def backtest_orchestrator(start_date,end_date,file_names,strategies,local_data,c
 
 if __name__ == "__main__":
     s3 = boto3.client('s3')
-    file_names = [
-         '2022-01-03', '2022-01-10', '2022-01-17', '2022-01-24', '2022-01-31', '2022-02-07', '2022-02-14', '2022-02-21', 
-         '2022-02-28', '2022-03-07', '2022-03-14', '2022-03-21', '2022-03-28', '2022-04-04', '2022-04-11', '2022-04-18', 
-         '2022-04-25', '2022-05-02', '2022-05-09', '2022-05-16', '2022-05-23', '2022-05-30', '2022-06-06', '2022-06-13', 
-         '2022-06-20', '2022-06-27', '2022-07-04', '2022-07-11', '2022-07-18', '2022-07-25', '2022-08-01', '2022-08-08', 
-         '2022-08-15', '2022-08-22', '2022-08-29', '2022-09-05', '2022-09-12', '2022-09-19', '2022-09-26', '2022-10-03', 
-         '2022-10-10', '2022-10-17', '2022-10-24', '2022-10-31', '2022-11-07', '2022-11-14', '2022-11-21', '2022-11-28', 
-         '2022-12-05', '2022-12-12', '2022-12-19', '2022-12-26', '2023-01-02', '2023-01-09', '2023-01-16', '2023-01-23', 
-         '2023-01-30', '2023-02-06', '2023-02-13', '2023-02-20', '2023-02-27', '2023-03-06', '2023-03-13', '2023-03-20', 
-         '2023-03-27', '2023-04-03', '2023-04-10', '2023-04-17', '2023-04-24', '2023-05-01', '2023-05-08', '2023-05-15', 
-         '2023-05-22', '2023-05-29', '2023-06-05', '2023-06-12', '2023-06-19', '2023-06-26', '2023-07-03', '2023-07-10', 
-         '2023-07-17', '2023-07-24', '2023-07-31', '2023-08-07', '2023-08-14', '2023-08-21', '2023-08-28', '2023-09-04', 
-         '2023-09-11', '2023-09-18', '2023-09-25', '2023-10-02', '2023-10-09', '2023-10-16', '2023-10-23', '2023-10-30', 
-         '2023-11-06'
-         ]
+    # '2022-01-03', '2022-01-10', '2022-01-17', '2022-01-24', '2022-01-31', '2022-02-07', '2022-02-14', '2022-02-21', 
+    #      '2022-02-28', '2022-03-07', '2022-03-14', '2022-03-21', '2022-03-28', '2022-04-04', '2022-04-11', '2022-04-18', 
+    #      '2022-04-25', '2022-05-02', '2022-05-09', '2022-05-16', '2022-05-23', '2022-05-30', '2022-06-06', '2022-06-13', 
+    #      '2022-06-20', '2022-06-27', '2022-07-04', '2022-07-11', '2022-07-18', '2022-07-25', '2022-08-01', '2022-08-08', 
+    #      '2022-08-15', '2022-08-22', '2022-08-29', '2022-09-05', '2022-09-12', '2022-09-19', '2022-09-26', '2022-10-03', 
+    #      '2022-10-10', '2022-10-17', '2022-10-24', '2022-10-31', '2022-11-07', '2022-11-14', '2022-11-21', '2022-11-28', 
+    #      '2022-12-05', '2022-12-12', '2022-12-19', '2022-12-26', 2023-10-16', '2023-10-23', '2023-10-30', 
+        #  '2023-11-06'
     # test_files = [ 
     #     '2023-01-02', '2023-01-09', '2023-01-16', '2023-01-23', 
     #      '2023-01-30', '2023-02-06', '2023-02-13', '2023-02-20', '2023-02-27', '2023-03-06', '2023-03-13', '2023-03-20', 
@@ -93,34 +86,131 @@ if __name__ == "__main__":
     #      '2023-05-22', '2023-05-29', '2023-06-05', '2023-06-12', '2023-06-19', '2023-06-26', '2023-07-03', '2023-07-10', 
     #      '2023-07-17', '2023-07-24', '2023-07-31', '2023-08-07', '2023-08-14', '2023-08-21', '2023-08-28', '2023-09-04', 
     #      '2023-09-11', '2023-09-18', '2023-09-25', '2023-10-02']
+    file_names = [
+         '2023-01-02', '2023-01-09', '2023-01-16', '2023-01-23', 
+         '2023-01-30', '2023-02-06', '2023-02-13', '2023-02-20', '2023-02-27', '2023-03-06', '2023-03-13', '2023-03-20', 
+         '2023-03-27', '2023-04-03', '2023-04-10', '2023-04-17', '2023-04-24', '2023-05-01', '2023-05-08', '2023-05-15', 
+         '2023-05-22', '2023-05-29', '2023-06-05', '2023-06-12', '2023-06-19', '2023-06-26', '2023-07-03', '2023-07-10', 
+         '2023-07-17', '2023-07-24', '2023-07-31', '2023-08-07', '2023-08-14', '2023-08-21', '2023-08-28', '2023-09-04', 
+         '2023-09-11', '2023-09-18', '2023-09-25', '2023-10-02', '2023-10-09'
+         ]
 
     test_files =  ['2023-08-14', '2023-08-21', '2023-08-28', '2023-09-04', 
     '2023-09-11', '2023-09-18', '2023-09-25', '2023-10-02']
 
+    test_files2 = ['2023-01-02', '2023-01-09', '2023-01-16', '2023-01-23', 
+         '2023-01-30', '2023-02-06', '2023-02-13', '2023-02-20', '2023-02-27', '2023-03-06', '2023-03-13', '2023-03-20', 
+         '2023-03-27', '2023-04-03', '2023-04-10', '2023-04-17', '2023-04-24']
+    
+    time_peridods = [test_files,test_files2,file_names]
 
-    start_date = '2023/08/14'
-    end_date = '2023/10/09'
-    start_str = start_date.split("/")[1] + start_date.split("/")[2]
-    end_str = end_date.split("/")[1] + end_date.split("/")[2]
-    strategies = ["BFC","BFC_1D","BFP","BFP_1D"]
+    for time in time_peridods:
+        start_dt = time[0]
+        end_date = time[-1]
 
-    backtest_configs = [{
-        "put_pct": 1, 
-        "spread_adjustment": 0,
-        "aa": 1,
-        "risk_unit": .002,
-        "vc": False,
-        "vc_level":"nvc",
-        "portfolio_cash": 200000,
-        "risk_adjustment": .003,
-        "probability": 0.5
-    }]
+        start_date = start_dt.replace("-","/")
+        end_dt = datetime.strptime(end_date, '%Y-%m-%d') + timedelta(days=7)
+        end_date = end_dt.strftime("%Y/%m/%d")
+        start_str = start_date.split("/")[1] + start_date.split("/")[2]
+        end_str = end_date.split("/")[1] + end_date.split("/")[2]
+        strategies = ["BFC","BFC_1D","BFP","BFP_1D"]
 
-    for config in backtest_configs:
-        trading_strat = f"modelsv2_noposlimit_{config['spread_adjustment']}out_{config['put_pct']}put_noresize_risk{config['risk_adjustment']}_AA{config['aa']}_{config['vc_level']}_prob{config['probability']}"
-        portfolio_df, positions_df = backtest_orchestrator(start_date, end_date,file_names=test_files,strategies=strategies,local_data=False, config=config) 
-        s3.put_object(Body=portfolio_df.to_csv(), Bucket="icarus-research-data", Key=f'backtesting_reports/{trading_strat}/{start_str}-{end_str}/{config["portfolio_cash"]}_{config["risk_unit"]}/portfolio_report.csv')
-        s3.put_object(Body=positions_df.to_csv(), Bucket="icarus-research-data", Key=f'backtesting_reports/{trading_strat}/{start_str}-{end_str}/{config["portfolio_cash"]}_{config["risk_unit"]}/positions_report.csv')
-        print(f"Done with {trading_strat}!")
+        backtest_configs = [
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 0,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": False,
+                "vc_level":"nvc",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .003,
+                "probability": 0.5
+            },
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 1,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": False,
+                "vc_level":"nvc",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .003,
+                "probability": 0.5
+            },
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 0,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": False,
+                "vc_level":"nvc",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .005,
+                "probability": 0.5
+            },
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 1,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": False,
+                "vc_level":"nvc",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .005,
+                "probability": 0.5
+            },
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 0,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": True,
+                "vc_level":"400$.003",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .005,
+                "probability": 0.5
+            },
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 0,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": True,
+                "vc_level":"400$.003",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .003,
+                "probability": 0.5
+            },
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 0,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": True,
+                "vc_level":"250$.003",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .005,
+                "probability": 0.5
+            },
+            {
+                "put_pct": 1, 
+                "spread_adjustment": 0,
+                "aa": 1,
+                "risk_unit": .002,
+                "vc": True,
+                "vc_level":"250$.003",
+                "portfolio_cash": 200000,
+                "risk_adjustment": .003,
+                "probability": 0.5
+            }
+        ]
+
+        for config in backtest_configs:
+            trading_strat = f"modelsv2_noposlimit_{config['spread_adjustment']}out_{config['put_pct']}put_noresize_risk{config['risk_adjustment']}_AA{config['aa']}_{config['vc_level']}_prob{config['probability']}"
+            portfolio_df, positions_df = backtest_orchestrator(start_date, end_date,file_names=test_files,strategies=strategies,local_data=False, config=config) 
+            s3.put_object(Body=portfolio_df.to_csv(), Bucket="icarus-research-data", Key=f'backtesting_reports/{trading_strat}/{start_str}-{end_str}/{config["portfolio_cash"]}_{config["risk_unit"]}/portfolio_report.csv')
+            s3.put_object(Body=positions_df.to_csv(), Bucket="icarus-research-data", Key=f'backtesting_reports/{trading_strat}/{start_str}-{end_str}/{config["portfolio_cash"]}_{config["risk_unit"]}/positions_report.csv')
+            print(f"Done with {trading_strat}!")
 
 
